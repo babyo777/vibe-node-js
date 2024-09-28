65a2d296ed7497c52d61c9b9aaeaa43053ac5dfb
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-09-28 21:44:36 +0530

    init

--- dist/src/models/roomModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-const roomSchema = new mongoose_1.default.Schema({}, { timestamps: true });
-const Room = mongoose_1.default.model("Room", roomSchema);
-exports.default = Room;
